dc0b2a9bb9069f91b7dced625ac601e3f92da763
iamrichid <rkklinks@gmail.com>
2024-03-07 22:47:07 +0000

    cold one

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import talky from '../images/hero.png'
+import ad1 from '../images/ad1.png'
 import support from '../images/section1.png'
 import anonymity from '../images/section2.png'
 
@@ -17,60 +17,25 @@ export default function Root() {
         </nav>
       </div> 
      
+      <div className="container">
+<div className="hero">
+        <article>
+          <h2>Talky</h2>
+          <p>Welcome to Talky, a platform where you can share your thoughts, 
+              feelings, and problems without the need to reveal your identity.</p> 
 
-        <div id="root-body">
-       
-        <div id="hero">
-        <div id="hero-text">
-           <h2>
-           Speak Freely, Without Judgment
-           </h2>
-           <p>Welcome to Talky, a platform where you can share your thoughts, 
-              feelings, and problems without the need to reveal your identity. 
-              Whether you're dealing with stress, anxiety, relationship issues,
-              or simply need someone to listen, we're here for you. 
-              Our anonymous support community is built on the principles of empathy,
-              understanding, and confidentiality. Join us to open up, be heard,
-              and receive support from our caring community of listeners.
-              Your privacy is our priority, and your voice matters, even anonymously.</p>
-        </div>
-        <img id='hero-image' src={talky}/>
+          <h3>Tank it</h3>
+          <p className='tanker'>whatever feelings are eating you up, we can tank it!</p>
+        
+         <img src={ad1} alt="" />
+
+         
+        </article>
         </div>
 
-        {/* First  Section */}
-        <div id="section">
-        <div id="section-text">
-           <h2>
-           Easy Process to Get Support
-           </h2>
-          <ol>
-            <li><b>Submit Your Problem:</b> Fill out our anonymous submission form with your thoughts, feelings, or problems.</li>
-            <li><b>Receive Empathetic Responses:</b> Our caring community of listeners will provide supportive responses and feedback.</li>
-            <li><b>Engage Anonymously:</b> Join the conversation, ask questions, or offer support to others without revealing your identity.</li>
-          </ol>
-        </div>
-        <img id='section-image' src={support}/>
+        
       </div>
-
- 
-
-        {/* second  Section */}
-        <div id="section1">
-        <div id="section1-text">
-           <h2>
-           The Power of Anonymity
-           </h2>
-          <p>Safe and Secure: Anonymity ensures your privacy and protects your identity, allowing you to share openly without fear of judgment.
-Greater Honesty: With anonymity, you can express yourself more honestly and authentically, leading to deeper and more meaningful conversations.
-Reduced Stigma: Anonymity removes barriers and stigma associated with seeking support, making it easier for individuals to reach out for help.</p>
-        </div>
-        <img id='section1-image' src={anonymity}/>
-      </div>
-
-      <footer id='buttom'>
-       <h4>© 2024 Talky.</h4>
-      </footer>
-      </div>
+        
      
 
       
